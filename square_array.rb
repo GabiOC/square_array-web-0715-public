a80ac6b7181328741d17375254dbd2e388cedd3d
GabiOC <oconnor.gabriele@gmail.com>
2015-07-30 17:09:40 -0400

    Add methods to square arrays

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |x|
+  	new_array << x**2
+  end
+  new_array
+end
+
+def collect_array(arr)
+	arr.collect { |y| y**2 }
 end
